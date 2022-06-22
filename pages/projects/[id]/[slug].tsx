cd57befdecac80f3e9b191df2390025cd36b88c9
Nevio <neviogomez91@gmail.com>
2022-06-22 17:51:28 -0400

    Moved stack to project

--- pages/projects/[id]/[slug].tsx
+++ pages/projects/[id]/[slug].tsx
@@ -4,9 +4,8 @@ import axios from 'axios'
 import Head from 'next/head'
 import Image from 'next/image'
 import NextLink from 'next/link'
-import { Box, Heading, Spinner, Text, Button } from '@chakra-ui/react'
+import { Box, SimpleGrid, Tooltip, Heading, Spinner, Text, Button } from '@chakra-ui/react'
 import fsPromises from 'fs/promises'
-import Stack from '../../../components/stack'
 import Layout from '../../../components/layouts/article'
 import path from 'path'
 import { ChevronLeftIcon } from '@chakra-ui/icons'
@@ -29,6 +28,83 @@ export type ProjectProps = {
     description?: string
     wikipage?: string
   }
+}
+const Stack: React.FC<ProjectProps> = (props) => {
+  const { project, stack } = props
+
+  return (
+    <Box textAlign='center' alignContent='center'>
+      <SimpleGrid columns={[2, 3, 3]} gap={4} justifyItems='center'>
+        {Array.isArray(project?.technologies) ? (
+          project?.technologies.map((item: string, index: number) =>
+            item.toLowerCase() !== 'tensorflow' ? (
+              <Tooltip
+                label={stack?.[item.toLowerCase()]?.description}
+                key={index}
+                fontSize='12px'
+                width='80%'
+              >
+                <Box width={150} my={4} key={index}>
+                  <a
+                    href={stack?.[item.toLowerCase()]?.wikipage}
+                    target='_blank'
+                  >
+                    <i
+                      className={`devicon-${
+                        item.toLowerCase().split('/')[0]
+                      }-plain colored`}
+                      style={{
+                        fontSize: '28px',
+                        textShadow: '1px 1px black',
+                      }}
+                    ></i>
+                    <Text textShadow='1px 1px black'>{item}</Text>
+                  </a>
+                </Box>
+              </Tooltip>
+            ) : (
+              <Tooltip
+                label={stack?.[item.toLowerCase()]?.description}
+                key={index}
+                fontSize='12px'
+                width='80%'
+              >
+                <Box width={150} my={4} key={index}>
+                  <a
+                    href={stack?.[item.toLowerCase()]?.wikipage}
+                    target='_blank'
+                  >
+                    <i
+                      className={`devicon-${item.toLowerCase()}-original colored`}
+                      style={{
+                        fontSize: '28px',
+                        textShadow: '1px 1px black',
+                      }}
+                    ></i>
+                    <Text textShadow='1px 1px black'>{item}</Text>
+                  </a>
+                </Box>
+              </Tooltip>
+            )
+          )
+        ) : (
+          <Tooltip
+            label={stack?.[project?.technologies.toLowerCase()]?.description}
+          >
+            <Box width={150} my={4}>
+              <a href={stack?.[project?.technologies.toLowerCase()]?.wikipage}>
+                <i
+                  className={`devicon-${project?.technologies.toLowerCase()}-plain colored`}
+                  style={{ fontSize: '20px' }}
+                ></i>
+                <Text textShadow='1px 1px black'>{project?.technologies}</Text>
+              </a>
+            </Box>
+          </Tooltip>
+        )}
+      </SimpleGrid>
+    </Box>
+  )
 }
 
 const Project: NextPage<ProjectProps> = ({ project, stack }) => {

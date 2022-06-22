6dd2581d62f4fbd26e6bef80893271c23eed65ad
Nevio <neviogomez91@gmail.com>
2022-06-22 18:22:29 -0400

    Fixing url routes

--- next.config.js
+++ next.config.js
@@ -15,7 +15,16 @@ const nextConfig = {
   exportPathMap: function () {
     return {
       '/': {page: '/'},
-      '/projects/1/klickwell/': {page: '/projects/[id]/[slug]'}
+      '/projects': {page: '/projects'},
+      '/projects/1/klickwell-ml': {page: '/projects/[id]/[slug]'},
+      '/projects/2/klickwell-live': {page: '/projects/[id]/[slug]'},
+      '/projects/3/klickwell': {page: '/projects/[id]/[slug]'},
+      '/projects/4/inspyro': {page: '/projects/[id]/[slug]'},
+      '/projects/5/baseshop': {page: '/projects/[id]/[slug]'},
+      '/projects/6/portfolionjs': {page: '/projects/[id]/[slug]'},
+      '/projects/7/portfolio-api': {page: '/projects/[id]/[slug]'},
+      '/projects/8/cryptopal': {page: '/projects/[id]/[slug]'},
+      '/projects/9/baseshopnjs': {page: '/projects/[id]/[slug]'},
     }
   }
 };
